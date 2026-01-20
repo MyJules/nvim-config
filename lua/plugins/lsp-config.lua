9b993c9fcc203fb3e73763105c284f405bd5d124
MyJules <mudryk200@gmail.com>
2026-01-20 20:37:11 +0100

    rm dash

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -2,7 +2,7 @@ return{
     {
         "mason-org/mason-lspconfig.nvim",
         opts = {
-            ensure_installed = { "lua_ls", "zls", "clangd" },
+            ensure_installed = { "lua_ls", "zls", "clangd", "rust_analyzer" },
         },
         dependencies = {
             { "mason-org/mason.nvim", opts = {} },
