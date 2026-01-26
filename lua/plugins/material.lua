964465226edb21075216e29d074f8460f0128b5d
MyJules <mudryk200@gmail.com>
2026-01-26 18:59:52 +0100

    material theme

---
+++ lua/plugins/material.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "marko-cerovac/material.nvim",
+    lazy = false,
+    name = "material",
+    priority = 1000,
+    config = function()
+      vim.g.material_style = "darker"
+      vim.cmd.colorscheme "material"
+    end
+  }
+}
