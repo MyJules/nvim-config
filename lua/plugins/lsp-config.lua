5854255673b65309fac2a56f9cc3cd64c19f5d79
MyJules <mudryk200@gmail.com>
2026-03-06 22:24:37 +0100

    Add cmake-tools plugin and enhance lsp-config with clang-tidy support

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,12 +1,30 @@
 return{
     {
         "mason-org/mason-lspconfig.nvim",
-        opts = {
-            ensure_installed = { "lua_ls", "zls", "clangd", "rust_analyzer" },
-        },
         dependencies = {
             { "mason-org/mason.nvim", opts = {} },
             "neovim/nvim-lspconfig",
         },
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "zls", "clangd", "rust_analyzer" },
+                handlers = {
+                    function(server_name)
+                        require("lspconfig")[server_name].setup({})
+                    end,
+                    clangd = function()
+                        require("lspconfig").clangd.setup({
+                            cmd = {
+                                "clangd",
+                                "--background-index",
+                                "--clang-tidy",
+                                "--completion-style=detailed",
+                                "--header-insertion=iwyu",
+                            },
+                        })
+                    end,
+                },
+            })
+        end,
     },
 }
