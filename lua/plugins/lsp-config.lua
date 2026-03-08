2f008f38f53d1e7761588370446f2f79c48b1960
MyJules <mudryk200@gmail.com>
2026-03-08 13:58:19 +0100

    Refactor LSP configuration by removing clangd setup and enabling automatic installation

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -7,23 +7,8 @@ return{
         },
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "zls", "clangd", "rust_analyzer" },
-                handlers = {
-                    function(server_name)
-                        require("lspconfig")[server_name].setup({})
-                    end,
-                    clangd = function()
-                        require("lspconfig").clangd.setup({
-                            cmd = {
-                                "clangd",
-                                "--background-index",
-                                "--clang-tidy",
-                                "--completion-style=detailed",
-                                "--header-insertion=iwyu",
-                            },
-                        })
-                    end,
-                },
+                ensure_installed = { "lua_ls", "zls","rust_analyzer" },
+                automatic_enable = true,
             })
         end,
     },
