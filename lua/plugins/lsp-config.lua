6e04691ace9e50d662600da00503669ab1977787
MyJules <mudryk200@gmail.com>
2026-06-02 21:44:06 +0200

    fix treesitter

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -13,12 +13,7 @@ return{
 
             local border = "rounded"
 
-            vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
-                vim.lsp.handlers.hover, { border = border }
-            )
-            vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
-                vim.lsp.handlers.signature_help, { border = border }
-            )
+            vim.o.winborder = border
             vim.diagnostic.config({
                 float = { border = border },
             })
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,13 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "master",
+  lazy = false,
   build = ":TSUpdate",
   config = function()
-    require'nvim-treesitter.configs'.setup {
+    require("nvim-treesitter.configs").setup({
       ensure_installed = { "lua", "cpp", "zig" },
       highlight = { enable = true },
       indent = { enable = true },
-    }
-  end
+    })
+  end,
 }
